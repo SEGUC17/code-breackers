9557abc91b489315c5da248acd17adf784dcfe68
nadyajdaa <nadyajdaa@gmail.com>
2017-04-03 22:20:01 +0600

    + Update and Delete

--- routes.js
+++ routes.js
@@ -24,4 +24,77 @@ var upload = multer ({ dest:'public/uploads'})
    console.log("addservice");
  });
 
+// UPDATE SERVICE ROUTES
+//PUT to update a blob by ID
+	router.put(function(req, res) {
+	    // Get our REST or form values. These rely on the "name" attributes
+	    var servicename = req.body.servicename;
+	    var description = req.body.description;
+	    var address = req.body.address;
+	    var price = req.body.price;
+      var location = req.body.location;
+      var category = req.body.category;
+
+	    //find the document by ID
+	    mongoose.model('service').findById(req.id, function (err, service) {
+	        //update it
+	        service.update({
+	            servicename : servicename,
+	            description : description,
+	            address : address,
+	            price : price,
+              location : location,
+              category : category
+
+	        }, function (err, serviceID) {
+	          if (err) {
+	              res.send("There was a problem updating the information to the database: " + err);
+	          }
+	          else {
+	                  //HTML responds by going back to the page or you can be fancy and create a new view that shows a success page.
+	                  res.format({
+	                      html: function(){
+	                           res.redirect("/serviceview/" + service._id);
+	                     },
+	                     //JSON responds showing the updated values
+	                    json: function(){
+	                           res.json(service);
+	                     }
+	                  });
+	           }
+	        })
+	    });
+	})
+  //DELETE a Service by ID
+router.delete('/:id/edit', function (req, res){
+    //find a Service by ID
+    mongoose.model('service').findById(req.id, function (err, service) {
+        if (err) {
+            return console.error(err);
+        } else {
+            //remove it from Mongo
+            service.remove(function (err, service) {
+                if (err) {
+                    return console.error(err);
+                } else {
+                    //Returning success messages saying it was deleted
+                    console.log('DELETE removing ID: ' + service._id);
+                    res.format({
+                        //HTML returns us back to the main or success page
+                          html: function(){
+                               res.redirect("/serviceView");
+                         },
+                         //JSON returns the item with the message that is has been deleted
+                        json: function(){
+                               res.json({message : 'deleted',
+                                   item : service
+                               });
+                         }
+                      });
+                }
+            });
+        }
+    });
+});
+
 module.exports = router;
