f01ee41abc6231ed677d06c880dca5a206fbe376
KareemAbdelAziz <kareem.abdellatef@student.guc.edu.eg>
2017-04-05 01:06:20 +0300

    Updated services

--- services.js
+++ services.js
@@ -1,28 +1,41 @@
-.service("BusinessData", [function () {
-    var businessData = [
-    {
-        id: 1,
-        serviceId: 1,
-        name: 'BreakOut',
-        distance: 0.1,
-        rating: 4
-    }
-];
+  //nadeen service schema
 
-    return {
-        getAllBusinesses: function () {
-            return businessData;
-        },
+var mongoose = require('mongoose');
+//require('./category');
+var serviceSchema = mongoose.Schema({
 
-        getSelectedBusiness: function(serviceId) {
-            var businessList = [];
-            serviceId = parseInt(serviceId);
-            for(i=0;i<businessData.length;i++) {
-                if(businessData[i].serviceId === serviceId) {
-                    businessList.push(businessData[i]);
-                }
-            }
-            return businessList;
-        }
-    }
-}])
+    servicename:{type:String, required:true, unique:true},
+    URL:String, //video's link
+    description : {type: String, required:true},
+    location:{type: mongoose.Schema.Types.ObjectId, ref: 'locationSchema'},
+    category:{type: mongoose.Schema.Types.ObjectId, ref: 'categorySchema'},
+    img:{type: mongoose.Schema.Types.ObjectId, ref: 'imageS'},
+    rating = [RSchema]
+});
+var RSchema= serviceSchema.find(servicename).populate('ratings._item').exec(function(err, user){
+if(err || !user)
+return next(new Error('Service username not found !!!!'));
+console.log(user.rating);
+});
+
+var Service = mongoose.model("service", serviceSchema);
+
+module.exports = Service;
+
+//     return {
+//         getAllBusinesses: function () {
+//             return businessData;
+//         },
+//
+//         getSelectedBusiness: function(serviceId) {
+//             var businessList = [];
+//             serviceId = parseInt(serviceId);
+//             for(i=0;i<businessData.length;i++) {
+//                 if(businessData[i].serviceId === serviceId) {
+//                     businessList.push(businessData[i]);
+//                 }
+//             }
+//             return businessList;
+//         }
+//     }
+// }])
