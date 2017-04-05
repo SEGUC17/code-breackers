4b6e8b723bbb4339e1a6db6d8b97c86e2600a1ad
SarahMohamed96 <SarahMohamed96@users.noreply.github.com>
2017-04-05 15:59:06 +0200

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -119,21 +119,58 @@ router.post('/change', function(req, res){
 				console.log("no such reservation");
 				}
 
-				//checking if a reservation is done --- omar and sarah
-				// check if service is opened on that
-
 			});
 				
 
-		
-
-
-		//req.flash('success_msg', 'You are registered and can now login');
-
-		//res.redirect('loginafter');
-	//}
 
 });
+
+
+
+
+
+
+router.get('/delete', function(req,res){
+res.render('delete.html', {title: "Delete Reservation"})
+});
+
+
+router.post('/delete', function(req, res){
+	var userName = req.body.userName;
+	var serviceName = req.body.serviceName;
+	var reservation_date = req.body.reservation_date;
+	var reservation_hour = req.body.reservation_hour;
+
+	//req.checkBody('userName', 'Username is required').notEmpty();
+	//req.checkBody('serviceName', 'ServiceName is required').notEmpty();
+	//req.checkBody('begin_work', 'begin_work is required').notEmpty();
+	//req.checkBody('end_work', 'end_work is required').notEmpty();
+	//req.checkBody('working_days', 'working_days is required').notEmpty();
+	//req.checkBody('reservation_date', 'reservation_date is required').notEmpty();
+
+
+	/*var errors = req.validationErrors();
+
+	if(errors){
+		res.redirect('/');
+	} else {*/
+		
+		Reservation.findOne({userName:req.body.userName,serviceName:req.body.serviceName,reservation_date: req.body.reservation_date,reservation_hour:
+				req.body.reservation_hour},function(err,trackReservation3){
+				if(trackReservation3){
+				Reservation.deleteReservation(trackReservation3, function(err){
+      				console.log("Deleted!");
+			});
+				if (!(trackReservation3)){
+				
+				console.log("No such reservation");
+				
+			}
+				
+
+
+ } });});
+
 
 
 module.exports = router;
