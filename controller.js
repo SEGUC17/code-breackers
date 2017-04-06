8d6365467f7784b34007a7ffdc88b38a37168be5
SarahMohamed96 <SarahMohamed96@users.noreply.github.com>
2017-04-06 19:14:20 +0200

    Revert "Kareem abdel aziz1"

--- controller.js
+++
@@ -1,8 +0,0 @@
-.controller('ServiceCtrl', function($scope, ServicesData, BusinessData, $stateParams)
- {
-  $scope.service = ServicesData.getSelectedService($stateParams.service);
-  $scope.businessList = BusinessData.getSelectedBusiness($stateParams.service);
-  $scope.ratings = {
-      current: 5,
-      max: 10
-     };
