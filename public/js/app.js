ea6523afcec335603559b0baf1af0e7d489559d1
Laila Madany <laila.madany@student.guc.edu.eg>
2017-04-20 22:59:56 +0200

    app.js

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+(function(){
+    angular.module('myApp', ['ui.router'])
+            .config(function($stateProvider, $urlRouterProvider){
+
+            $urlRouterProvider.otherwise('/');
+
+            $stateProvider
+                .state('search', {
+                url: "/search",
+                templateUrl: "public/views/search.html",
+                controller: "SearchController"
+            })
+
+            .state('service', {
+            url: "/service/:id",
+            templateUrl: "public/views/service.html",
+            controller: "ServiceController"
+        })
+
+          .state('home', {
+           url: "/",
+          templateUrl: "public/views/home.html",
+          controller: "SearchController"
+
+         })
+
+
+
+
+        })
+}());
