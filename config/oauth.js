3bb0005eae387fa832205df3ac1f011085200d06
SarahMohamed96 <SarahMohamed96@users.noreply.github.com>
2017-04-27 15:15:33 +0200

    Delete oauth.js

--- config/oauth.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-// facebookAuth: {
-//    clientID: process.env.FB_CLIENT_ID,
-//    clientSecret: process.env.FB_SECRET,
-//    callbackURL: process.env.FB_CALLBACK_URL,
-//    enableProof: false
-//   }
-// }
